278353d4b229240c123da1e7ceee7381a7b2594a
Josue Figueroa <poor.rican@pm.me>
2023-01-27 12:00:18 -0800

    Implemented `Poller.interval`

--- src/polling.rs
+++ src/polling.rs
@@ -10,6 +10,8 @@ use crate::io::IOEvent;
 pub struct Poller<T: 'static, K: Eq + Hash + 'static> {
     interval: Duration,
     last_execution: DateTime<Utc>,
+
+    // internal containers
     sensors: &'static Container<Box<dyn Sensor<T>>, K>,
     log: &'static mut Container<IOEvent<T>, DateTime<Utc>>,
 }
@@ -18,11 +20,10 @@ impl<T, K: Eq + Hash> Poller<T, K> {
     /// Iterate through container once. Call `get_event()` on each value.
     /// Update according to the lowest rate.
     fn poll(&mut self) {
-        for sensor in self.sensors._inner().values() {
-            let timer: Duration = sensor.get_metadata().min_delay;
-            let next_execution = self.last_execution + timer;
+        let next_execution = self.last_execution + self.interval;
 
-            if next_execution <= Utc::now() {
+        if next_execution <= Utc::now() {
+            for sensor in self.sensors._inner().values() {
                 self.last_execution = next_execution;
                 let event = sensor.get_event(next_execution);
                 self.log.add(next_execution, event);
