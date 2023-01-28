d2617cc9fa83f029ab60da867fe43a297c19b141
Josue Figueroa <poor.rican@pm.me>
2023-01-27 16:49:51 -0800

    Update `Settings` to include interval

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,14 @@
+use chrono::Duration;
 use dotenv::dotenv;
 
+/// Default values
 const VERSION: &str = "0.0.1-alpha";
+const INTERVAL: i32 = 10;
 
+/// Struct containing settings loaded from ".env"
 pub struct Settings {
     pub version: String,
+    pub interval: Duration,
 }
 
 impl Settings {
@@ -11,6 +16,7 @@ impl Settings {
     pub fn initialize() -> Self {
         dotenv().ok();
         let version = std::env::var("VERSION").unwrap_or_else(|_| VERSION.to_string());
-        Settings { version }
+        let interval =  Duration::seconds(i64::from(std::env::var("INTERVAL").unwrap_or(INTERVAL.to_string())));
+        Settings { version, interval }
     }
 }
