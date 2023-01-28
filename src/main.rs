a907757becbb2b613e41673eca00e14b1a17e523
Josue Figueroa <poor.rican@pm.me>
2023-01-27 16:50:55 -0800

    Implemented `Poller.interval`

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,35 @@ mod sensors;
 mod settings;
 mod units;
 
-use chrono::Duration;
+use chrono::{DateTime, Duration, Utc};
 
 use crate::container::{Collection, Container, Containerized};
 use crate::device::Sensor;
+use crate::polling::Poller;
 use crate::sensors::ph::MockPhSensor;
 use crate::settings::Settings;
 use crate::units::Ph;
 
 fn main() {
-    let _settings = Settings::initialize();
+    static SETTINGS: Settings = Settings::initialize();
+    unsafe {
+        static mut SENSORS: Container<Box<dyn Sensor<Ph>>, i32> = <dyn Sensor<Ph>>::container();
+        static mut LOG: Container<io::IOEvent<Ph>, DateTime<Utc>> = <io::IOEvent<Ph>>::container();
+        static mut POLLER: Poller<Ph, i32> = Poller::new(
+            &settings.interval,
+            Utc::now() - settings.interval,
+            &mut SENSORS,
+            &mut LOG
+        );
+    }
+
 
     let s0 = MockPhSensor::new("test name".to_string(), 0, Duration::seconds(5));
     let s1 = MockPhSensor::new("second sensor".to_string(), 1, Duration::seconds(10));
-    let mut container: Container<Box<dyn Sensor<Ph>>, i32> = <dyn Sensor<Ph>>::container();
+
     container.add(0, Box::new(s0));
     container.add(1, Box::new(s1));
+
     dbg!(container._inner());
+
 }
--- src/polling.rs
+++ src/polling.rs
@@ -8,18 +8,18 @@ use crate::io::IOEvent;
 /// Mediator that polls a `Container` of `Sensors` and populates another container with `IOEvent` objects.
 /// TODO: multithreaded polling
 pub struct Poller<T: 'static, K: Eq + Hash + 'static> {
-    interval: Duration,
+    interval: &'static Duration,
     last_execution: DateTime<Utc>,
 
     // internal containers
-    sensors: &'static Container<Box<dyn Sensor<T>>, K>,
+    sensors: &'static mut Container<Box<dyn Sensor<T>>, K>,
     log: &'static mut Container<IOEvent<T>, DateTime<Utc>>,
 }
 
 impl<T, K: Eq + Hash> Poller<T, K> {
     /// Iterate through container once. Call `get_event()` on each value.
     /// Update according to the lowest rate.
-    fn poll(&mut self) {
+    pub fn poll(&mut self) {
         let next_execution = self.last_execution + self.interval;
 
         if next_execution <= Utc::now() {
@@ -29,5 +29,11 @@ impl<T, K: Eq + Hash> Poller<T, K> {
                 self.log.add(next_execution, event);
             }
         }
+    }
+
+    pub fn new( interval: &'static Duration, last_execution: DateTime<Utc>,
+            sensors: &'static mut Container<Box<dyn Sensor<T>>, K>,
+            log: &'static mut Container<IOEvent<T>, DateTime<Utc>> ) -> Self {
+        Self { interval, last_execution, sensors, log }
     }
 }
