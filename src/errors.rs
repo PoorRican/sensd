1495f4ed06f40a54d54257bd3435b9f5c5c982a1
Josue Figueroa <poor.rican@pm.me>
2023-02-02 21:08:58 -0800

    Change `Error` to `ErrorKind`

--- src/errors.rs
+++ src/errors.rs
@@ -3,48 +3,38 @@ use std::fmt;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+pub enum ErrorKind {
+    ContainerError,
+    ContainerEmpty,
+    ContainerNotEmpty,
+    SerializationError,
+}
+
 #[derive(Debug)]
-pub enum Error {
-    ContainerEmpty(Option<String>),
-    ContainerNotEmpty(Option<String>),
-    SerializationError(Option<String>),
+pub struct Error {
+    kind: ErrorKind,
+    message: String,
 }
 
 impl Error {
-    pub fn new(error_type: Error, message: &str) -> Box<dyn _Error> {
-        let msg = String::from(message);
-        Box::new( match error_type {
-            Self::ContainerEmpty(ref _msg) => Self::ContainerEmpty(Some(msg)),
-            Self::ContainerNotEmpty(ref _msg) => Self::ContainerNotEmpty(Some(msg)),
-            Self::SerializationError(ref _msg) => Self::SerializationError(Some(msg)),
-        })
+    pub fn new(kind: ErrorKind, msg: &str) -> Box<dyn _Error> {
+        let message = String::from(msg);
+        Box::new( Error { kind, message })
     }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Self::ContainerEmpty(ref message) => {
-                if let Some(ref message) = message {
-                    write!(f, "Container is empty: {}", message)
-                } else {
-                    write!(f, "Container is empty")
-                }
-            },
-            Self::ContainerNotEmpty(ref message) => {
-                if let Some(ref message) = message {
-                    write!(f, "Container is not empty: {}", message)
-                } else {
-                    write!(f, "Container is not empty")
-                }
-            },
-            Self::SerializationError(ref message) => {
-                if let Some(ref message) = message {
-                    write!(f, "Error from Serializer: {}", message)
-                } else {
-                    write!(f, "Error during serialization")
-                }
-            }
+        let pretext = match self.kind {
+            Self::ContainerError => "Error from `Container` method",
+            Self::ContainerEmpty => "Container is empty",
+            Self::ContainerNotEmpty => "Container is not empty",
+            Self::SerializationError => "Error during serialization",
+        };
+
+        match self.message {
+            Some(message) => write!(f, "{}: {}", pretext, message),
+            _ => write!(f, pretext),
         }
     }
 }
