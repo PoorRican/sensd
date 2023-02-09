680278e1e317cf994937ae9b732d685ff5bb0467
Josue Figueroa <poor.rican@pm.me>
2023-02-09 15:48:28 -0800

    Add sensor/log builder function to helpers.rs

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,14 +1,18 @@
 use std::fs::File;
+use std::ops::Deref;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 
 use crate::errors::Result;
+use crate::io::{IdType, InputType, MockPhSensor, Device, InputDevice, DeviceType};
+use crate::settings::Settings;
+use crate::storage::OwnedLog;
 
-pub fn write_or_create(path: &Path) -> File {
-    File::options().write(true).open(path)
+pub fn write_or_create(path: String) -> File {
+    File::options().write(true).open(path.deref())
         .unwrap_or_else(move |_| {
-            File::create(path).unwrap();
-            File::options().write(true).open(path).unwrap()
+            File::create(path.deref()).unwrap();
+            File::options().write(true).open(path.deref()).unwrap()
         })
 }
 
@@ -24,3 +28,19 @@ pub fn check_results(results: &[Result<()>]) -> Result<()> {
 
 // Defines a type alias `Deferred` for an Arc wrapped around a Mutex with generic type T.
 pub type Deferred<T> = Arc<Mutex<T>>;
+
+pub trait Deferrable<T> {
+    fn deferred(&self) -> Deferred<T>;
+}
+
+/// Init sensor and `OwnedLog`, then set owner on log. Return log and sensor.
+pub fn input_log_builder(name: &str, id: IdType, settings: Arc<Settings>) -> (Deferred<OwnedLog>, Deferred<InputType>) {
+    let log = Arc::new(Mutex::new(OwnedLog::new(id, settings)));
+    let sensor = MockPhSensor::new(name.to_string(), id, log.clone());
+
+    let wrapped = sensor.deferred();
+    log.lock().unwrap().set_owner(wrapped.clone());
+
+
+    (log, wrapped)
+}
